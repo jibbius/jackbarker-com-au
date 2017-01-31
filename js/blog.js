36400e7e6b5f49504b26a18e6021149098a644f2
Jack Barker <jack.barker.au@gmail.com>
2017-01-31 12:00:48 +1100

    working skeleton

--- js/blog.js
+++ js/blog.js
@@ -1,55 +1,3 @@
-$(function() {
-//   var ref = new Firebase("https://jack-barker-blog.firebaseio.com/"),
-//     postRef = ref.child(slugify(window.location.pathname));
-    var postRef = firebase.database().ref('/posts');
-
-    postRef.on('child_added', function(snapshot) {
-      var newPost = snapshot.val();
-      $(".comments").prepend('<div class="comment">' +
-        '<h4>' + newPost.title + '</h4>' +
-        '<div class="profile-image"><img src="http://www.gravatar.com/avatar/' + newPost.uid + '?s=100&d=retro"/></div> ' +
-        '<span class="date">' + moment(newPost.starCount).fromNow() + '</span><p>' + newPost.body  + '</p></div>');
-    });
-
-    $("#comment").submit(function() {
-        
-        writeNewPost(
-            md5($("#email").val()),     // uid
-            $("#name").val(),           // username
-            md5($("#email").val()),     // picture
-            '',                         // title
-            $("#message").val()         // body
-        );
-    //   $("input[type=text], textarea").val("");
-      return false;
-    });
-
-});
-
-
-function writeNewPost(uid, username, picture, title, body) {
-  // A post entry.
-  var postData = {
-    author: username,
-    uid: uid,
-    body: body,
-    title: title,
-    starCount: 0,
-    authorPic: picture
-  };
-
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('posts').push().key;
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/posts/' + newPostKey] = postData;
-  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-  return firebase.database().ref().update(updates);
-}
-
-
 
 function slugify(text) {
   return text.toString().toLowerCase().trim()
@@ -58,4 +6,62 @@ function slugify(text) {
     .replace(/[^a-zA-Z0-9-_]+/g,'');
 }
 
- 
+ var post_uid = slugify(window.location.pathname);
+
+$(function() {
+    var db = firebase.database();
+    var commentRef = db.ref('/posts/' + post_uid + '/comments/');
+        
+        // Get the comment(s)
+    commentRef.on('child_added', function(snapshot) {
+      var newComment = snapshot.val();
+
+      $(".comments").append(
+        '<div class="comment">' +
+          '<h4>' + newComment.author + '</h4>' +
+          '<div class="profile-image">' +
+            '<img src="http://www.gravatar.com/avatar/' + newComment.author_email_md5 + '?s=100&d=retro"/>' +
+          '</div> ' +
+          '<span class="date">' + moment(newComment.timestamp).fromNow() + '</span>' +
+          '<p>' + newComment.body + '</p>' +
+        '</div>');
+    });
+
+    $("#comment").submit(function() {      
+        writeNewComment(
+            post_uid,                   // post_uid
+            $("#name").val(),           // author
+            $("#email").val(),          // author_email
+            $("#message").val()         // comment body
+        );
+
+        // Clear the text input
+      $("input[type=text], textarea").val("");
+      return false;
+    });
+});
+
+
+function writeNewComment(post_uid, author, author_email, comment_body) {
+  // A comment entry.
+  var commentData = {
+    post_uid: post_uid,
+    timestamp: firebase.database.ServerValue.TIMESTAMP,
+
+    author: author,
+    author_email: author_email,
+    author_email_md5: md5(author_email),
+
+    title: '',
+    body: comment_body
+  };
+
+  var updates = {};
+
+  // Get a key for a new Comment.
+  var newCommentKey = firebase.database().ref('/posts/' + post_uid + '/comments/').child('comments').push().key;
+  
+  // Write the new comment to the post's comments list.
+  updates['/posts/' + post_uid + '/comments/' + newCommentKey] = commentData;
+  return firebase.database().ref().update(updates);
+}
