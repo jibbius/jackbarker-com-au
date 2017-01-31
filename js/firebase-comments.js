e7e2638a77bdb04c7588ba1f899d3166bca2ea1e
Jack Barker <jack.barker.au@gmail.com>
2017-02-01 09:45:55 +1100

    work in progress

--- js/firebase-comments.js
+++ js/firebase-comments.js
@@ -75,48 +75,13 @@ FirebaseComments.prototype.saveMessage = function(e) {
   }
 };
 
-// Sets the URL of the given img element with the URL of the image stored in Firebase Storage.
-FirebaseComments.prototype.setImageUrl = function(imageUri, imgElement) {
-  imgElement.src = imageUri;
-
-  // TODO(DEVELOPER): If image is on Firebase Storage, fetch image URL and set img element's src.
-};
-
-// Saves a new message containing an image URI in Firebase.
-// This first saves the image in Firebase storage.
-FirebaseComments.prototype.saveImageMessage = function(event) {
-  var file = event.target.files[0];
-
-  // Clear the selection in the file picker input.
-  this.imageForm.reset();
-
-  // Check if the file is an image.
-  if (!file.type.match('image.*')) {
-    var data = {
-      message: 'You can only share images',
-      timeout: 2000
-    };
-    // this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
-    console.log(data);
-    return;
-  }
-  // Check if the user is signed-in
-  if (this.checkSignedInWithMessage()) {
-
-    // TODO(DEVELOPER): Upload image to Firebase storage and add message.
-
-  }
-};
-
-// Signs-in Friendly Chat.
+// Signs-in
 FirebaseComments.prototype.signIn = function() {
   // Sign in Firebase using popup auth and Google as the identity provider.
   var provider = new firebase.auth.GoogleAuthProvider();
   this.auth.signInWithPopup(provider);
 };
-
-
-// Signs-out of Friendly Chat.
+// Signs-out
 FirebaseComments.prototype.signOut = function() {
   // Sign out of Firebase.
   this.auth.signOut();
@@ -125,9 +90,10 @@ FirebaseComments.prototype.signOut = function() {
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 FirebaseComments.prototype.onAuthStateChanged = function(user) {
   if (user) { // User is signed in!
+
     // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = user.photoURL; // Only change these two lines!
-    var userName = user.displayName;   // Only change these two lines!
+    var profilePicUrl = user.photoURL;
+    var userName = user.displayName;
 
     // Set the user's profile pic and name.
     this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
@@ -140,10 +106,8 @@ FirebaseComments.prototype.onAuthStateChanged = function(user) {
 
     // Hide sign-in button.
     this.signInButton.setAttribute('hidden', 'true');
-
-    // We load currently existing chant messages.
-    this.loadMessages();
   } else { // User is signed out!
+      
     // Hide user's profile and sign-out button.
     this.userName.setAttribute('hidden', 'true');
     this.userPic.setAttribute('hidden', 'true');
@@ -152,6 +116,10 @@ FirebaseComments.prototype.onAuthStateChanged = function(user) {
     // Show sign-in button.
     this.signInButton.removeAttribute('hidden');
   }
+
+    // Regardless, we load currently existing chant messages.
+    this.loadMessages();
+
 };
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
@@ -166,7 +134,8 @@ FirebaseComments.prototype.checkSignedInWithMessage = function() {
     message: 'You must sign-in first',
     timeout: 2000
   };
-//   this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
+
+  // TODO: render on screen   
   console.log(data);
   return false;
 };
@@ -174,7 +143,6 @@ FirebaseComments.prototype.checkSignedInWithMessage = function() {
 // Resets the given MaterialTextField.
 FirebaseComments.resetMaterialTextfield = function(element) {
   element.value = '';
-  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
 };
 
 // Template for messages.
@@ -204,27 +172,18 @@ FirebaseComments.prototype.displayMessage = function(key, name, text, picUrl, im
   }
   div.querySelector('.name').textContent = name;
   var messageElement = div.querySelector('.message');
-  if (text) { // If the message is text.
-    messageElement.textContent = text;
-    // Replace all line breaks by <br>.
-    messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
-  } else if (imageUri) { // If the message is an image.
-    var image = document.createElement('img');
-    image.addEventListener('load', function() {
-      this.messageList.scrollTop = this.messageList.scrollHeight;
-    }.bind(this));
-    this.setImageUrl(imageUri, image);
-    messageElement.innerHTML = '';
-    messageElement.appendChild(image);
-  }
+  messageElement.textContent = text;
+  
+  // Replace all line breaks by <br>.
+  messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
+
   // Show the card fading-in.
   setTimeout(function() {div.classList.add('visible')}, 1);
   this.messageList.scrollTop = this.messageList.scrollHeight;
   this.messageInput.focus();
 };
 
-// Enables or disables the submit button depending on the values of the input
-// fields.
+// Enables or disables the submit button depending on the values of the input fields.
 FirebaseComments.prototype.toggleButton = function() {
   if (this.messageInput.value) {
     this.submitButton.removeAttribute('disabled');
@@ -236,10 +195,10 @@ FirebaseComments.prototype.toggleButton = function() {
 // Checks that the Firebase SDK has been correctly setup and configured.
 FirebaseComments.prototype.checkSetup = function() {
   if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
+    console.log('You have not configured and imported the Firebase SDK. ' +
         'Make sure you go through the codelab setup instructions.');
   } else if (config.storageBucket === '') {
-    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+    console.log('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
         'actually a Firebase bug that occurs rarely. ' +
         'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
         'and make sure the storageBucket attribute is not empty. ' +
