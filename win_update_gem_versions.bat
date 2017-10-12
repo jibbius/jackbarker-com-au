8b546d11b4aafc380a85346b2d3947833f4d54ed
Jack Barker <jack.barker.au@gmail.com>
2017-10-12 23:36:54 +1100

    updated footer & misc changes

--- win_update_gem_versions.bat
+++ win_update_gem_versions.bat
@@ -1,7 +1,7 @@
 @REM ------------
 @REM Download updated versions JSON
 @REM ------------
-bitsadmin  /transfer mydownloadjob  /download  /priority normal https://pages.github.com/versions.json  %cd%\versions.json
+REM bitsadmin  /transfer mydownloadjob  /download  /priority normal https://pages.github.com/versions.json  %cd%\versions.json
 
 @REM ------------
 @REM Update bundles
